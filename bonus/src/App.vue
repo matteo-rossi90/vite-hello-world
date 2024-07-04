2129b6ded17676c6dbac35d985792f2574803311
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-07-04 18:44:12 +0200

    feat(bonus): add jumbotron AppHero

---
+++ bonus/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
+
+
+export default{
+  components: {
+    AppTitle,
+    AppHero,
+  }
+}
+</script>
+
+<template>
+  <AppHero/>
+  <AppTitle/>
+  
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ bonus/src/components/AppHero.vue
@@ -0,0 +1,28 @@
+<script>
+    
+    export default{
+        name: "AppHero" // nome componente
+    }
+
+</script>
+
+<template>
+    <header>
+        <h2>Ciao, sono un jumbotron</h2>
+    </header>
+</template>
+
+<style scoped>
+    /* stile css */
+
+    header{
+        background-color: dodgerblue;
+        height: 200px;
+        width: 100%;
+    }
+
+    h2{
+        color: white;
+        line-height: 200px;
+    }
+</style>
